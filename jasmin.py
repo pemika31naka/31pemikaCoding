cfda6de645c2b5323712ec9790a853d70538dfdd
pemika31naka <26952@bbw.ac.th>
2026-06-11 14:23:02 +0700

    โปนแกรมคำนวณค่าใช้จ่าย และ โปรแกรมพุดและตัวแปร
    11 มิถุนายน 2569
    ชอบที่ครูเฟิร์นสอนแบบนี้ค่ะทำให้เข้าใจง่ายมากค่ะ อยากให้สอนประมาณนี้เลยค่ะ ถ้าเร็วไปอาจตามไม่ทันได้ค่ะ การสอนครั้งนี้มีความสนุกค่ะเข้าใจง่ายค่ะเข้าใจโค้ดพุดและตัวแปรมากขึ้น รักค่ะรักครูเฟิร์นค่ะอิอิ

---
+++ jasmin.py
@@ -0,0 +1,7 @@
+name = input("คุณชื่ออะไร")
+age = int(input("อายุเท่าไหร่ ? "))
+next_age = age +1
+height = int(input("ส่วนสูง(เมตร)"))
+print("สวัสดี" , name )
+print(" ปีหน้าคุณจะอายุ", next_age  , "ปี")
+print("ส่วนสูงของคุณคือ" , height, "เมตร")
