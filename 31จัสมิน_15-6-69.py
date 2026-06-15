d93b79002df095cba132127315af22c731665da8
pemika31naka <26952@bbw.ac.th>
2026-06-15 09:44:46 +0700

    15.6.69
    ตั้งแต่เปิดเรียนมาได้ฝึกเขียนโค้ดpython ผ่านโปรแกรม vs code มากมายค่ะ สนุกมากค่ะ วันนี้ได้ฝึกเขียนโปรแกรมตรวจเลขค่ะ เข้าใจง่ายค่ะ

---
+++ 31จัสมิน_15-6-69.py
@@ -0,0 +1,8 @@
+print ("โปรแกรมเลขคู่หรือเลขคี่กันนะ")
+number = int(input ("enter your number :  "))
+
+if number % 2 == 0 :
+    print("เลขคู่")
+else : 
+    print("เลขคี่")   
+print( "by jasmin 4/4"  )
