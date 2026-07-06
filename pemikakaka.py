a6840adb2736eef18738bfc99dac5dbf4e808147
pemika31naka <26952@bbw.ac.th>
2026-07-06 09:05:47 +0700

    Add files via upload
    6/7/2569 วันนี้หนูได้เรียนการเขียนแม่สูตรคูณเดี่ยวโดยใช้ for loop ค่ะ

---
+++ pemikakaka.py
@@ -0,0 +1,7 @@
+print("โปรแกรมเขียนสูตรคูณแม่เดี่ยวค่ะ  \n ")
+num = int(input("กรอกเลขของคุณ   :  "))
+
+for i in range (1,13) :
+    print(num, "x" ,i, "=" ,num*i)
+
+print (" \n By pemika 4/4 no.31")
